c82cfd19ae9562043b4496db207edb92ec571a39
erin-maxson <erin@cohatch.com>
2022-10-07 17:22:59 -0700

    updating connection + thought controller

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/myfirstmongooseapp', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmediaapi', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
